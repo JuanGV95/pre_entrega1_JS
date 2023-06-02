d9bbd6c18f55805fa626f79f24efcc311a0b8164
Juan David Gonzalez <juandagv95@gmail.com>
2023-06-01 20:58:14 -0500

    añado comentarios para entender mejor el ejercicio

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 //este es un ejemplo que quiero desarrollar para mi proyecto final
 //la idea es un aplicativo con el que se pueda llevar registro de ingresos
 //egresos y mas adelante planeo añadir funciones para control de gastos
-//impuestos y cuotas faltantes para pagar
+//impuestos y cuotas faltantes para 
+//let cuenta es el valor general de la cuenta
 let cuenta = 0;
+//con esta funcion inicializamos el proceso, se debe ingresar un valor para poder acceder al menu
 function nombre() {
     let nombreI = prompt("Ingresa tu nombre");
     if(nombreI == ""){
@@ -13,14 +15,17 @@ function nombre() {
 function saludar(persona) {
     alert(`Bienvenido/a ${persona} ¿Qué deseas hacer?`);
 }
+//Esta funcion nos dice cual es el valor actual en la cuenta que por defecto es 0 cero
 function revSaldo() {
     alert(`Su saldo actual es de ${cuenta} USD`);
 }
+//Con esta funcion podemos consignar un valor a nuesta cuenta inicial y lo actualiza con su nuevo valor
 function consignacion() {
     let valorIng = parseInt(prompt("Digita el valor a consignar"));
     cuenta = valorIng + cuenta;
     alert(`Su nuevo saldo es de ${cuenta} USD`);
 }
+//Con esta funcion podemos retirar valor de nuetra cuenta base 
 function retirando() {
     let valorRes = parseInt(prompt("Digita el valor a retirar"));
     if (valorRes > cuenta) {
@@ -31,7 +36,8 @@ function retirando() {
         alert(`Su nuevo saldo es de ${cuenta} USD`);
     }
 }
-
+//El Menu fue realizado en base al ejemplo visto en clase 
+//(si es posible necesito un feedbak de como hacer funcionar mejor el condicional del if)
 nombre();
 if (salida = true) {
 
